ed54941a3128a058d7ea60a5d578e06ecbbe1405
unknown <admuni@WST220d.unibel.by>
2021-07-23 15:53:01 +0300

    loaded

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+
+            <img src="https://bipbap.ru/wp-content/uploads/2017/08/2.jpg" />
+
+            <div>
+                av + description
+        </div>
+            <div>
+                My post
+                <div> New post</div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
